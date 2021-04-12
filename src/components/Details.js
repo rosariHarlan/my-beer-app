ed5a79ab5cba7e983f486180f249aff64956c500
Rosari Harlan <rosari.harlan@gmail.com>
2021-04-12 18:41:50 +0200

    Fixed useEffect

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,56 +1,54 @@
-import { Link, withRouter } from "react-router-dom";
-import { getBeer } from "./../BeerAPI";
-import { Component } from "react";
-import arrow from "./../arrow.png";
+import { useParams, Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-class Details extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      oneBeer: [],
-    };
+import arrow from "./../arrow.png";
+
+const _test = () => {
+  let { id } = useParams();
+  const [oneBeer, setOneBeer] = useState(null);
+  useEffect(() => {
+    fetch("https://ih-beers-api2.herokuapp.com/beers/" + id)
+      .then((res) => res.json())
+      .then((res) => setOneBeer(res));
+  }, [id]);
+
+  if (!oneBeer) {
+    return <p>Loading...</p>;
   }
 
-  componentDidMount() {
-    getBeer(this.props.match.params.id).then((data) =>
-      this.setState({ oneBeer: data })
-    );
-  }
+  const {
+    image_url,
+    name,
+    tagline,
+    first_brewed,
+    attenuation_level,
+    description,
+  } = oneBeer;
 
-  render() {
-    const {
-      image_url,
-      name,
-      tagline,
-      first_brewed,
-      attenuation_level,
-      description,
-    } = this.state.oneBeer;
-    return (
-      <section id="details">
-        <div className="details">
-          <img src={image_url} alt={name} />
-          <h3>{name}</h3>
-          <p>{tagline}</p>
-          <p>
-            First brewed: <span>{first_brewed}</span>
-          </p>
-          <p>
-            Attenuation level: <span>{attenuation_level}</span>
-          </p>
-          <p>{description}</p>
-          <div className="arrow-container">
-            <span className="arrow">
-              <Link to="/all">
-                <img src={arrow} alt="arrow" />
-              </Link>
-            </span>
-          </div>
+  return (
+    <section id="details">
+      <div className="details">
+        <img src={image_url} alt={name} />
+        <h3>{name}</h3>
+        <p>{tagline}</p>
+        <p>
+          First brewed: <span>{first_brewed}</span>
+        </p>
+        <p>
+          Attenuation level: <span>{attenuation_level}</span>
+        </p>
+        <p>{description}</p>
+        <div className="arrow-container">
+          <span className="arrow">
+            <Link to="/all">
+              <img src={arrow} alt="arrow" />
+            </Link>
+          </span>
         </div>
-        <Navbar />
-      </section>
-    );
-  }
-}
+      </div>
+      <Navbar />
+    </section>
+  );
+};
 
-export default withRouter(Details);
+export default _test;
